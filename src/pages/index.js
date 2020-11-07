c3e84472aa9c8318b814f0369a79e687c012c408
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-11-07 16:35:40 +0500

    Adding Lolly SVG

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React from "react"
+import Lolly from "../components/Lolly"
+// import IMG from '../svg/lolly-image.svg'
+
+
 
 export default function Home() {
 
 
   return (<div>
     Hello World
+<div>
+  <Lolly />
+</div>
   </div>)
 }
